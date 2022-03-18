11eb48596da99929b610fca26003ed2990c35aa8
Vadim Khoroshev <vs.khoroshev@gmail.com>
2022-03-18 19:17:20 +0300

    Инициализировать проект

---
+++ desktop/src/ru/vkhoroshev/spacewars/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package ru.vkhoroshev.spacewars;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.vkhoroshev.spacewars.GameApp;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        new Lwjgl3Application(new GameApp(), config);
+    }
+}
